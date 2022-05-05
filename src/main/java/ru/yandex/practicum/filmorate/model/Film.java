73545cb7b11ab0af6844e7276c646bc2532ab04e
Kate Mahova <kate.mahova@yandex.ru>
2022-05-05 18:34:46 +0300

    fix: models

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.Builder;
 import lombok.Data;
-import javax.validation.constraints.*;
 
 import java.time.Duration;
 import java.time.LocalDate;
@@ -11,15 +10,8 @@ import java.time.LocalDate;
 @Builder
 public class Film {
     private int id; // целочисленный идентификатор
-    @NotNull
-    @NotEmpty
     private final String name; // название
-    @NotNull
-    @Size(max = 200)
     private final String description; // описание
-    @NotNull
     private final LocalDate releaseDate; // дата релиза
-    @Positive
-    @NotNull
     private final Duration duration; // продолжительность фильма
 }
